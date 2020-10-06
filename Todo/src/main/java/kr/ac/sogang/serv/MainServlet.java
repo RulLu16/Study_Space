eda19fac45770dda1ae851a0bd23c9233bdf14bd
RulLu16 <may05200@naver.com>
2020-10-06 16:07:57 +0900

    todo list. use servlet, jsp. need to clean code..

---
+++ Todo/src/main/java/kr/ac/sogang/serv/MainServlet.java
@@ -0,0 +1,60 @@
+package kr.ac.sogang.serv;
+
+import java.io.IOException;
+import java.util.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.ac.sogang.java.*;
+
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		List<TodoDto> dto=new ArrayList<>();
+		TodoDao dao=new TodoDao();
+		dto=dao.getTodos();
+		
+		String path = "/main.jsp";
+		RequestDispatcher dipatcher = request.getRequestDispatcher(path);
+		
+		request.setAttribute("todo", dto);
+		dipatcher.forward(request, response);
+		//System.out.println("dd");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		doGet(request, response);
+	}
+
+}
--- chap2/src/main/java/chap2/Main.java
+++ chap2/src/main/java/chap2/Main.java
@@ -9,9 +9,11 @@ public class Main {
 		
 		AnnotationConfigApplicationContext ctx= //자바 설정에서 정보 읽어와 빈 객체 생성, 관리
 				new AnnotationConfigApplicationContext(AppContext.class); //AppContext.class를 생성자 파라미터로 전달
-		Greeter g=ctx.getBean("greeter",Greeter.class); //greeter 객체 검색, getBean(빈 객체 이름, 빈 객체 타입)
-		String msg=g.greet("스프링");
+		Greeter g1=ctx.getBean("greeter",Greeter.class); //greeter 객체 검색, getBean(빈 객체 이름, 빈 객체 타입)
+		Greeter g2=ctx.getBean("greeter",Greeter.class);
+		String msg=g1.greet("스프링");
 		System.out.println(msg);
+		System.out.println("(g1==g2): "+(g1==g2)); // 싱글톤 객체. 스프링은 별도 설정 없을 시 한개의 빈 객체만 생성.
 		ctx.close();
 	}
 
