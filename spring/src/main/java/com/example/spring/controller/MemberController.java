94027a44e0b5294b8615fbd2268a59564c3284b9
RulLu16 <may05200@naver.com>
2020-12-29 16:59:36 +0900

    구동 페이지 작성하기

--- spring/src/main/java/com/example/spring/controller/MemberController.java
+++ spring/src/main/java/com/example/spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.spring.controller;
 
+import com.example.spring.domain.Member;
 import com.example.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,4 +20,27 @@ public class MemberController {
         this.memberService = memberService;
     } // bean 등록된 것들에서 가져와서 주입
     // 생성자 주입이 처음 만들때 주입 후 수정 불가능하므로 가장 많이 쓰이는 방법
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    } // submit post 방식으로 넘어오는 정보를 처리.
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
 }
