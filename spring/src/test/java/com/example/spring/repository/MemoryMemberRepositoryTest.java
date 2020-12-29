f25d0dbd492a716fa7abc773bda1d25bf3b3a2fb
RulLu16 <may05200@naver.com>
2020-12-29 14:56:43 +0900

    간단한 회원정리 시스템. 정보를 저장할 member를 정의하고 test를 작성해보기

---
+++ spring/src/test/java/com/example/spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.spring.repository;
+
+import com.example.spring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    } // test는 순서가 보장되지 않으므로 끝날때마다 clear하여 중복을 막는것이 좋다
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        //Assertions.assertEquals(member, result); // 제대로 생성되었는지 test
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
