52e3fc2ad93b3d1fb9dafb17d7acf2a65a51dc49
RulLu16 <may05200@naver.com>
2020-12-29 15:26:41 +0900

    service 객체 생성 및 test 작성

---
+++ spring/src/main/java/com/example/spring/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.example.spring.service;
+
+import com.example.spring.domain.Member;
+import com.example.spring.repository.MemberRepository;
+import com.example.spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member){
+        // 중복 회원 검사
+        validDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("exist name!");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
