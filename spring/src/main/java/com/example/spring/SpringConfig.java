a2429079ae7d94179e84757df61f7bd68d63180f
RulLu16 <may05200@naver.com>
2020-12-30 23:21:15 +0900

    spring data jpa 사용해보기

--- spring/src/main/java/com/example/spring/SpringConfig.java
+++ spring/src/main/java/com/example/spring/SpringConfig.java
@@ -17,25 +17,32 @@ public class SpringConfig {
     @Autowired
     public SpringConfig(DataSource dataSource){
         this.dataSource = dataSource;
-    }*/
+    }
 
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    }*/
     // 설정파일을 둘 경우, 상황에 따라 구현 클래스를 변경해야 할 때 매우 편리
 }
