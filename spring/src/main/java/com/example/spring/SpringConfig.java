ffc114cc6e30afc3718d9bab867f71eca1d582f6
RulLu16 <may05200@naver.com>
2020-12-30 16:43:09 +0900

    예전 방식인 jdbc 적용해보기

--- spring/src/main/java/com/example/spring/SpringConfig.java
+++ spring/src/main/java/com/example/spring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example.spring;
 
+import com.example.spring.repository.JdbcMemberRepository;
 import com.example.spring.repository.MemberRepository;
 import com.example.spring.repository.MemoryMemberRepository;
 import com.example.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration // 자바 코드로 직접 스프링 빈 등록
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
     // 설정파일을 둘 경우, 상황에 따라 구현 클래스를 변경해야 할 때 매우 편리
 }
