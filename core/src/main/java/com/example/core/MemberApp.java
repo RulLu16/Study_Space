363d26a2c3c36884f167034d885ecc8c0f063551
RulLu16 <may05200@naver.com>
2021-01-02 15:55:33 +0900

    인프런-스프링핵심원리 예제 코드 시작

---
+++ core/src/main/java/com/example/core/MemberApp.java
@@ -0,0 +1,17 @@
+package com.example.core;
+
+import com.example.core.member.Grade;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import com.example.core.member.Member;
+
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+    }
+}
---
+++ core/src/test/java/com/example/core/Member/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.core.Member;
+
+
+import com.example.core.member.Grade;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.example.core.member.Member;
+
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
