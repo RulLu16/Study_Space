f3d01ec489fc64f536135d360f0fefaa1a1243bb
RulLu16 <may05200@naver.com>
2021-01-14 16:42:06 +0900

    수동 등록과 자동 등록에서의 충돌 처리

--- core/src/main/java/com/example/core/AutoAppConfig.java
+++ core/src/main/java/com/example/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.example.core;
 
+import com.example.core.member.MemberRepository;
+import com.example.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,10 @@ import org.springframework.context.annotation.FilterType;
         // 이미 있던 AppConfig와 충돌을 피하기 위해 임의로 exclude
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    } // 수동 등록과 자동등록이 충돌하면 수동 빈 등록이 우선권을 가져 이미 생성된 자동 빈을 override
+    // 하지만 최근에는 수동과 자동이 충돌하면 에러를 띄우도록 바뀌고 있음.
 }
