4efe0b4930dc3aa2e8fceb48145297b1012ec478
RulLu16 <may05200@naver.com>
2021-01-14 21:55:22 +0900

    autowired의 다양한 주입 방법

--- core/src/main/java/com/example/core/order/OrderServiceImpl.java
+++ core/src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 @Component("orderService") // 빈 이름 지정도 가능
 public class OrderServiceImpl implements OrderService{
 
+    // @Autowired // 필드 자동 의존주입.
+    // 외부에서 변경이 불가능해 테스트가 힘들고, DI 프레임워크가 없으면 아무것도 못함 => 사용 하지 말자..
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
     // 변경하려면 서비스 코드를 고쳐야하는 문제 ->  OCP, DIP같은 객체지향 설계원칙 준수가 x
@@ -19,11 +21,22 @@ public class OrderServiceImpl implements OrderService{
     // 이처럼 아예 인터페이스에만 의존하게 해서 설계원칙을 준수 -> but, 구현체를 못 넣으니 에러.
     // 그러므로 누군가가 대신 만들어서 주입해주어야 함.
 
-    @Autowired
+    @Autowired // 생성자가 하나면 생략해도 스프링 빈 사용시 자동 주입해줌.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    /*@Autowired // setter를 통해 의존 자동 주입
+    public void setMemberRepository(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }*/
+
+    /*@Autowired // 일반 메서드에 의존 자동 주입. 거의 사용하지 않음.
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
