bbf8da28f72777087b196e49dac2a6dd22324753
RulLu16 <may05200@naver.com>
2021-01-17 16:33:50 +0900

    생명주기 처음 initial과 종료될 시 처리할 이벤트들을 관리

--- core/src/test/java/com/example/core/lifecycle/NetworkClient.java
+++ core/src/test/java/com/example/core/lifecycle/NetworkClient.java
@@ -1,31 +1,45 @@
 package com.example.core.lifecycle;
 
-public class NetworkClient { // 임시 네트워크
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean { // 임시 네트워크
 
     private String url;
 
-    public NetworkClient(){
+    public NetworkClient() {
         System.out.println("call url");
 
-        connect();
-        call("init message");
     }
 
-    public void setUrl(String url){
+    public void setUrl(String url) {
         this.url = url;
     }
 
     // call when service start
-    public void connect(){
+    public void connect() {
         System.out.println("connect");
     }
 
-    public void call(String message){
+    public void call(String message) {
         System.out.println("call and message: " + message);
     }
 
     // call when service end
-    public void disconnect(){
+    public void disconnect() {
         System.out.println("close");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입 이후 initial 하는 부분을 여기에 넣음
+        connect();
+        call("init message");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // 빈이 종료될 때 호출
+        disconnect();
     }
 }
