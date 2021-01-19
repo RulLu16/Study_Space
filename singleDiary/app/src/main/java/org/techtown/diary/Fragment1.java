7a1ecfc36254e59d53b740a9ecbc58639104a714
RulLu16 <may05200@naver.com>
2021-01-19 13:33:01 +0900

    첫번째 화면 구현. 뭐가뭔지 아직 모르겠다..

--- singleDiary/app/src/main/java/org/techtown/diary/Fragment1.java
+++ singleDiary/app/src/main/java/org/techtown/diary/Fragment1.java
@@ -1,25 +1,105 @@
 package org.techtown.diary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import lib.kingja.switchbutton.SwitchMultiButton;
 
 public class Fragment1 extends Fragment {
+
+    RecyclerView recyclerView;
+    NoteAdapter adapter;
+
+    Context context;
+    OnTabItemSelectedListener listener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+
+        if(context instanceof  OnTabItemSelectedListener){
+            listener = (OnTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        if (context != null) {
+            context = null;
+            listener = null;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
         initUI(rootView);
 
         return rootView;
     }
 
-    private void initUI(ViewGroup viewGroup){
-        // 인플레이션 후 xml 레이아웃 안의 위젯이나 레이아웃을 찾아 변수에 할당하는 코드 입력
+
+    private void initUI(ViewGroup rootView) {
+
+        Button todayWriteButton = rootView.findViewById(R.id.todayWriteButton);
+        todayWriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onTabSelected(1);
+                }
+            }
+        });
+
+        SwitchMultiButton switchButton = rootView.findViewById(R.id.switchButton);
+        switchButton.setOnSwitchListener(new SwitchMultiButton.OnSwitchListener() {
+            @Override
+            public void onSwitch(int position, String tabText) {
+                Toast.makeText(getContext(), tabText, Toast.LENGTH_SHORT).show();
+
+                adapter.switchLayout(position);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        adapter = new NoteAdapter();
+
+        adapter.addItem(new Note(0, "0", "Goyang", "", "","happy", "0", "capture1.jpg", "02/10"));
+        adapter.addItem(new Note(1, "1", "Goyang", "", "","fun", "0", "capture1.jpg", "02/11"));
+        adapter.addItem(new Note(2, "0", "Goyang", "", "","sad..", "0", null, "02/13"));
+
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnNoteItemClickListener() {
+            @Override
+            public void onItemClick(NoteAdapter.ViewHolder holder, View view, int position) {
+                Note item = adapter.getItem(position);
+
+                Toast.makeText(getContext(), "item selected : " + item.getContents(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
