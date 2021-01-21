a7110bab12d78bf7a506ab9dc7b9a30e5782f030
RulLu16 <may05200@naver.com>
2021-01-21 21:30:47 +0900

    proxy를 사용해 request의 주입을 관리해보기

--- core/src/main/java/com/example/core/common/MyLogger.java
+++ core/src/main/java/com/example/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package com.example.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,9 +9,11 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 // 에러 발생. 왜? => request scope는 실제 요청이 들어 와야 생성됨. 그 이전에 주입을 시도하면 생성 안된 빈이므로 에러가 발생
 // 그럼 요청 들어오고 빈이 만들어질 때 주입을 하자 => provider의 사용이 대두됨
+// proxy mode : MyLogger의 가짜 프록시 클래스를 만들어두고,
+// http request와 상관없이 가짜 프록시 클래스를 다른 빈에 미리 주입이 가능. => provider를 사용하지 않아도 동작하게 됨.
 public class MyLogger {
 
     private String uuid;
--- core/src/main/java/com/example/core/web/LogDemoController.java
+++ core/src/main/java/com/example/core/web/LogDemoController.java
@@ -14,15 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
     // DL을 사용해 주입 시점에 받도록 수정
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
-        MyLogger myLogger = myLoggerProvider.getObject();
+        //MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
+        // 실제 호출하면 가짜 프록시 객체의 메서드를 호출하고, 가짜 프록시 객체는 진짜 mylogger를 찾아호출한다.
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
